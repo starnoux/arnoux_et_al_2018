15d57aec26ef287ba633c865baf321f6fe502af7
starnoux <ste.arnoux@gmail.com>
2018-06-12 14:23:24 +0200

    Update 09_SplitFilterParalog_vbeta.R

--- 3.Paralog_Cleaning/09_SplitFilterParalog_vbeta.R
+++ 3.Paralog_Cleaning/09_SplitFilterParalog_vbeta.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 
 #title : 'Split and Filter the Paralogous sites'
-#author: "script from J. David. 1/03/2012, adapted to our analyses by S. Arnoux"
+#author: " Script from Sylvain Glémin (sylvain.glemin@univ-montp2.fr) / C++ version included in the read2snp software, J. David. 1/03/2012, adapted to our analyses by S. Arnoux"
 #date : "January 2018"
 
 ### DEFINITION OF ARGUMENTS 
